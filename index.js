7581755d367acc910ca7e92264d1394a913dc844
sample-text-here <yesthisisbot@gmail.com>
2021-01-06 19:02:30 -0800

    working prototype

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 // the main server
-
-// load the modules
-const express = require("express"); // the actual server
-const compression = require("compression"); // make the webpage s p e e e d
-const app = express(); // create the app
-app.use(compression());
-app.use(express.static("/public"));
-
-const listener = app.listen(3000);
-
-const io = require("socket.io")(listener);
+const { abs } = require(__dirname + "/lib/util.js");
+require(abs("./lib/routes.js"))(abs);
